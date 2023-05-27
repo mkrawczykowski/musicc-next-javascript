12a1c5b6d6659694cb39c2602bef5ba0b8638920
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-05-27 10:45:12 +0200

    RM

--- src/components/structure/Context/Context.jsx
+++ src/components/structure/Context/Context.jsx
@@ -15,29 +15,3 @@ export const ContextProvider = ({ children }) => {
         </Context.Provider>
     );
 };
-
-
-
-
-
-
-
-// import React, { createContext, useState } from 'react';
-
-// export const Context = createContext();
-
-// const initialState = {
-//     activeMegaMenu: [0],
-//     mobileMenuActive: false
-// };
-
-// const Store = ({ children }) => {
-//     const [state, setState] = useState(initialState);
-//     console.log(' Store state')
-//     console.log(state)
-//     return (
-//         <Context.Provider value={{ state, setState }}>{children}</Context.Provider>
-//     )
-// }
-
-// export default Store;
