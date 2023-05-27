044fd389c3df90c746c3cf506d4c7556b9ceb1bd
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-05-27 15:22:12 +0200

    header on mobile - proper positions

--- src/components/structure/MainHeader/MainHeader.jsx
+++ src/components/structure/MainHeader/MainHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useRef } from 'react';
 import styles from './MainHeader.module.scss';
-import { Container } from '../Grid/Grid';
+import { Container, Row } from '../Grid/Grid';
 import Logo from '../Logo/Logo';
 import Search from '../Search/Search';
 import MainMenu from '../MainMenu/MainMenu';
@@ -8,12 +8,14 @@ import Hamburger from '../Hamburger/Hamburger';
 
 const MainHeader = () => {
     return (
-        <div className={styles.mainHeader} classes={'containerHeaderColumns'}>
+        <div className={styles.mainHeader}>
             <Container>
-                <Search />
-                <Logo />
-                <Hamburger />
-                <MainMenu />
+                <Row classes={'rowHeaderColumns'}>
+                    <Search />
+                    <Logo />
+                    <Hamburger />
+                    <MainMenu />
+                </Row>
             </Container>
         </div>
     )
